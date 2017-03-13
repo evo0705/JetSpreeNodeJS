ea0a0bc1c0eec69869b68cad471b7da1eae5da38
samuel.lee <samuel.lee@tropicanacorp.com.my>
2017-03-13 15:48:08 +0800

    seperate main & sub categories

--- routes/categories.js
+++ routes/categories.js
@@ -6,8 +6,17 @@ router
 	/* GET list of Categories */
 	.get('/', function (req, res) {
 		var db = req.db;
-		var collection = db.get('categories');
+		var collection = db.get('categories').find({"main": null});
 			
+		collection.find({}, {}, function (e, docs) {
+			res.json(docs);
+		});
+	})
+	
+	/* GET list of Sub Categories */
+	.get('/sub', function (req, res) {
+		var db = req.db;
+		var collection = db.get('categories').find({"main": {$ne: null} });			
 		collection.find({}, {}, function (e, docs) {
 			res.json(docs);
 		});
