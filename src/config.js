a8baa8984732764e7ffd9ae24e83fb5452d43d5d
samuel.lee <samuel.lee@tropicanacorp.com.my>
2017-03-22 14:36:18 +0800

    no message

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ module.exports = {
 	'token_duration': 60 * 60 * 24, // expires in 24 hours
 	'facebook_app_id': (process.env.NODE_ENV == 'production' ? '757488797737661' : '650580988428443'),
 	'facebook_app_secret': (process.env.NODE_ENV == 'production' ? '89bd709ff644ab4afb56ecc0b239c07d' : '88574244ad4e8a659a213ca3b18f98c3'),
-	'facebook_callback_url': "/login/facebook/callback"
+	'facebook_callback_url': (process.env.NODE_ENV == 'production' ? 'https://jetspree-node-test.herokuapp.com/login/facebook/callback' : "/login/facebook/callback")
 };
