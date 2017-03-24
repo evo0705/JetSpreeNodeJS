4f0d589ad7ab5f5333d2d643c556a784d7901e3a
samuel.lee <samuel.lee@tropicanacorp.com.my>
2017-03-24 16:52:44 +0800

    add gulp-cache

--- gulpfile.js
+++ gulpfile.js
@@ -4,49 +4,59 @@ const notify = require('gulp-notify');
 const livereload = require('gulp-livereload');
 const babel = require('gulp-babel');
 const clean = require('gulp-clean');
+const Cache = require('gulp-file-cache');
+
+var cache = new Cache();
 
 gulp.task('default', ['watch']);
 
 gulp.task('compile', function () {
-	// Compile everything
-	return gulp.src('dist', { read: false })
-		.pipe(clean())
+
+	// gulp.src('dist', { read: false })
+	// 	.pipe(clean())
+	// 	.on('end', function () {
+
+	return gulp.src('./src/**/*.js')
+		.pipe(cache.filter())
+		.pipe(babel({
+			presets: ['es2015', 'stage-0']
+		}))
+		.pipe(cache.cache())
+		.pipe(gulp.dest('./dist'))
+		.pipe(livereload())
 		.on('end', function () {
 
-			gulp.src('./src/**/*.js')
-				.pipe(babel({
-					presets: ['es2015', 'stage-0']
-				}))
-				.pipe(gulp.dest('./dist'))
+			gulp.src('./src/views/**/*.pug')
+				.pipe(gulp.dest('./dist/views'))
 				.pipe(livereload())
 				.on('end', function () {
 
-					gulp.src('./src/views/**/*.pug')
-						.pipe(gulp.dest('./dist/views'))
-						.pipe(livereload())
-						.on('end', function () {
-
-							gulp.src('./src/public/**/*')
-								.pipe(gulp.dest('./dist/public'))
-								.pipe(livereload());
-						});
+					gulp.src('./src/public/**/*')
+						.pipe(gulp.dest('./dist/public'))
+						.pipe(livereload());
 				});
 		});
+	// });
 })
 
+var nodemon_instance;
 gulp.task('watch', function () {
 	// livereload for browser
 	livereload.listen();
 
 	// configure nodemon
-	nodemon({
-		tasks: ['compile'],
-		script: 'dist/app',
-		ext: 'js',
-		watch: 'src'
-	}).on('restart', function () {
-		gulp.src('dist/app.js')
-			.pipe(livereload())
-			.pipe(notify('Reloading Page due to code changes'));
-	})
+	if (!nodemon_instance) {
+		nodemon_instance = nodemon({
+			tasks: ['compile'],
+			script: 'dist/app',
+			ext: 'js',
+			watch: 'src'
+		}).on('restart', function () {
+			gulp.src('dist/app.js')
+				.pipe(livereload());
+				//.pipe(notify('Reloading Page due to code changes'));
+		});
+	} else {
+		nodemon_instance.emit('restart');
+	}
 })
