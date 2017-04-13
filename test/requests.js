4570382ea620dcd6503f55fd66b6fc207e19bb78
Samuel Lee <evo0705@gmail.com>
2017-04-13 14:51:48 +0800

    correct gitignore dist/

--- test/requests.js
+++ test/requests.js
@@ -5,10 +5,10 @@ let server = require('../dist/app');
 chai.use(chaiHttp);
 
 let token = '';
-describe("Route /login/account", () => {
+describe("Route /v1/login/account", () => {
     it("should respond", (done) => {
         chai.request(server)
-            .post('/login/account')
+            .post('/v1/login/account')
             .send({email: 'blackyu123@gmail.com', password: 'qwe123'})
             .end((err, res) => {
                 basicSuccess(res);
@@ -18,10 +18,10 @@ describe("Route /login/account", () => {
     });
 });
 
-describe("Route /auth/user", () => {
+describe("Route /v1/auth/user", () => {
     it("should respond", (done) => {
         chai.request(server)
-            .get('/auth/user')
+            .get('/v1/auth/user')
             .set('x-access-token', token)
             .end((err, res) => {
                 basicSuccess(res);
@@ -30,10 +30,10 @@ describe("Route /auth/user", () => {
     });
 });
 
-describe("Route /requests", () => {
+describe("Route /v1/requests", () => {
     it("should respond", (done) => {
         chai.request(server)
-            .get('/requests')
+            .get('/v1/requests')
             .end((err, res) => {
                 basicSuccess(res);
                 done();
