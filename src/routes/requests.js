82428c31daa1f873da0061b96cf653c0a6c76717
DIJAYACORP\j.donald <xetro90@gmail.com>
2017-04-21 10:18:05 +0800

    added pagination for request

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -10,18 +10,32 @@ router
         let queryFrom = '';
         let queryWhere = '';
         let queryParams = [];
+	
+		let page = parseInt(req.query.page, 10);
+		if (isNaN(page) || page < 1) { page = 1; }
+	
+		let pageSize = parseInt(req.query.pageSize, 10);
+		if (isNaN(pageSize) || pageSize < 1) {
+			pageSize = 10;
+		} else if (pageSize > 10) {
+			pageSize = pageSize;
+		}
+	
+		var offset = (page - 1) * pageSize;
+        
         if (req.query.name) {
-            queryFrom += ", to_tsvector(name) AS the_field, plainto_tsquery($" + (queryParams.length + 1) + ") AS the_words";
+            queryFrom += ", to_tsvector(name) AS the_field, plainto_t[squery($" + (queryParams.length + 1) + ") AS the_words";
             queryWhere += " AND the_field @@ the_words";
             queryParams.push(req.query.name);
         }
+        
         if (req.query.id) {
             queryWhere += " AND id=$" + (queryParams.length + 1);
             queryParams.push(req.query.id);
         }
 
         req.pool.connect().then(client => {
-            client.query('SELECT * FROM items' + queryFrom + ' WHERE 1=1' + queryWhere, queryParams)
+            client.query('SELECT * FROM items OFFSET $1 LIMIT $2' + queryFrom + ' WHERE 1=1' + queryWhere, queryParams, [offset, pageSize])
                 .then(result => {
                     client.release();
                     return res.json({
