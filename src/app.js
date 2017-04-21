c271f417818716744a897ba2d8924a0c9a9b20a2
DIJAYACORP\j.donald <xetro90@gmail.com>
2017-04-21 10:20:25 +0800

    without next param it will throw error res.status not found and the error page will not work

--- src/app.js
+++ src/app.js
@@ -136,18 +136,18 @@ app.use((req, res, next) => {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use((err, req, res) => {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+	app.use((err, req, res, next) => {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
